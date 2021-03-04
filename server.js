200c544cb5f1b13257fd6132043b036fcaa740d8
Morris-Desktop <morteza.hazrati@gmail.com>
2021-03-04 23:33:03 +1100

    A get message service created in the node and implemented in the frontend to populate the messages div.

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+var messages = [
+  { name: "Tim", message: "Hi Jane, how are you?" },
+  { name: "Jane", message: "I am really fine! How are you?" },
+];
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
 const server = app.listen(3000, () => {
   console.log("server is listening on port ", server.address().port);
 });
